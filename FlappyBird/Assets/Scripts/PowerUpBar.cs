0f5defc2ec7889af514e37c97ea0044118d4a184
dudemaiaf <dudu_maia13@hotmail.com>
2020-09-17 22:58:43 -0400

    projeto

---
+++ FlappyBird/Assets/Scripts/PowerUpBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpBar : MonoBehaviour
+{
+    public Slider slider;
+    public float FillSpeed = 0.5f;
+    public float targetProgress = 0;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+    
+    void Start ()
+    {
+        IncrementProgress(1f);
+    }
+
+    void Update ()
+    {
+        if(slider.value < targetProgress)
+        {
+            slider.value += FillSpeed * Time.deltaTime;
+        }else{
+            StartCoroutine(DecrementProgress());
+        }
+    }
+
+    IEnumerator DecrementProgress()
+    {
+        yield return new WaitForSeconds(3f);
+        slider.value = 0;
+    }
+
+    public void IncrementProgress(float newProgress)
+    {
+        targetProgress = slider.value + newProgress;
+    }
+}
---
+++ FlappyBird/Assets/Scripts/addScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addScore : MonoBehaviour
+{
+    GameObject a;
+    public PowerUpBar power;
+    void Start()
+    {
+        a = GameObject.Find("PowerUp");
+
+        power = a.GetComponent<PowerUpBar>();
+    }
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(power.slider.value >= 1)
+        {
+            Score.score++;
+            Score.score++;
+        }else{
+            Score.score++;
+        }
+    }
+}
